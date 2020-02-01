69677a165a819b53822950d85d4ea74584c653a7
dolphinos7 <bbern0140@gmail.com>
2020-02-01 00:29:41 -0500

    Functioning Twitter Puller, Outputs List Of Strings

--- Twitter.py
+++ Twitter.py
@@ -1,15 +1,26 @@
 from twython import Twython
-APP_KEY = 'zI15R3JccQKrtPR1l9siC4K9q'
-APP_SECRET = 'lQQCH7ZLusQqJC8QSk3CG5DS7tTzWHHjgkpdasd0he3a4JTBLe'
+import datetime
+def GetTweets(searchTerm):
+    APP_KEY = 'zI15R3JccQKrtPR1l9siC4K9q'
+    APP_SECRET = 'lQQCH7ZLusQqJC8QSk3CG5DS7tTzWHHjgkpdasd0he3a4JTBLe'
+    twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
+    ACCESS_TOKEN = twitter.obtain_access_token()
+    twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
+    query=searchTerm
+    searchResults = []
+    tweetTextList = []
+    for x in range (0, 14):
+        searchResults.append(twitter.search(q=query, result_type='popular', until=str(datetime.date.today()-datetime.timedelta(x))))
+    for x in range(0, len(searchResults)):
+        for y in range(0, len(searchResults[x].get('statuses'))):
+            initialText = searchResults[x].get('statuses')[y].get('text')
+            endIndex = initialText.find("https")
+            linklessText = initialText[0:endIndex]
+            tweetTextList.append(linklessText)
+    return tweetTextList
 
-twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
-ACCESS_TOKEN = twitter.obtain_access_token()
-twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
-twitter.search(q='python', result_type='popular')
-print (twitter.search(q='python', result_type='popular'))
-#query = {'q': 'learn python',
- #       'result_type': 'popular',
-  #      'count': 10,
-   #     'lang': 'en',
-    #    }
-#python_tweets.search(query='python', result_type='popular')urned a 401 (Unauthorized), Could not authenticate you.
+
+print(GetTweets('iran'))
+
+
+
