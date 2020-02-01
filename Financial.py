072d7c93774955857292816ae6c56828bb798a2e
Matt Davison <mattd7@vt.edu>
2020-01-31 20:35:16 -0500

    sample multithreading

--- Financial.py
+++ Financial.py
@@ -1,2 +1,23 @@
+import concurrent.futures
+import os
+from queue import Queue
+
+
 def finQuery(query):
-    return ["Test"]
+    ret = []
+
+    def scrapeBloomberg():
+        #do scraping here
+        scrapeData = ""
+        return scrapeData
+    
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        #code to start each scrape as a thread
+        bloomberg = executor.submit(scrapeBloomberg)
+        #end add thread
+
+        #add results to return string
+        ret.append(bloomberg.result())
+        
+    return ret
+    
