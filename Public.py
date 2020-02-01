5f23fa83781628b4b63b2d5183ea4a507ebded5d
Matt Davison <mattd7@vt.edu>
2020-01-31 22:55:12 -0500

    added multithreaded demo for each category

--- Public.py
+++ Public.py
@@ -1,2 +1,17 @@
+import concurrent.futures
+
 def pubQuery(query):
-    return ["Test"]
+    def scrapeReddit():
+        print("todo")
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        #code to start each scrape as a thread
+        reddit = executor.submit(scrapeReddit)
+        #end add thread
+
+        #add results to return string
+        ret = []
+        ret.append(reddit.result())
+
+
+        return ret
