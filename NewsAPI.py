b718e227e53696738a3acb4db6d61a6c3d9cf283
Ryan Gniadek <ryan@gniadek.net>
2020-02-01 08:56:12 -0500

    FULLY WORKING NEWS SCRAPPER

--- NewsAPI.py
+++ NewsAPI.py
@@ -1,6 +1,17 @@
 from newsapi import NewsApiClient
 
-api = NewsApiClient(api_key='64c70df4109b49529da4fa0dbbf949e1')
+def GetNews(searchTerm):
+    api = NewsApiClient(api_key='64c70df4109b49529da4fa0dbbf949e1')
 
-print(api.get_everything(q='Iran').get('title'))
+    all_headlines = ''
 
+    for pageNum in range(1,6):
+        all_articles = api.get_everything(q=searchTerm, page=pageNum).get('articles')
+
+        for article in all_articles:
+            headline = article.get('title')
+            all_headlines = all_headlines+headline
+
+    return all_headlines
+
+print(GetNews('China'))
